6837ef24840319cdab0ac6ba123a7e09ec6358fb
mahdis-fathi <88924157+mahdis-fathi@users.noreply.github.com>
2023-12-13 22:14:11 +0330

    the first design of the models

---
+++ ShopStore/ShopStore/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using ShopStore.Models.Context;
+
+namespace ShopStore.Models
+{
+    public class Cart
+    {
+        public int CardId { get; set; }
+        public string CardName { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public int OrderId { get; set; }
+        public User User { get; set; }
+        public List<Product> Products { get; set; }
+        public List<CartItem> CartItems { get; set; }
+    }
+}
