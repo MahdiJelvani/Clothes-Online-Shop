7fe1c0eb0cd01b116f123d85a0ce22957009d985
amaz2 <zahramansouri1010@gmail.com>
2023-12-15 14:32:42 +0330

    login & signup
    header is ok
    but footer have a little bug so I comment it

---
+++ WebApplication1/WebApplication1/Controllers/IdentityController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class IdentityController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IdentityController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult signup()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
